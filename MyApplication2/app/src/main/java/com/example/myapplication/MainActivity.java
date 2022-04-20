30eb748e567b5a2771cf215f5e121de7c1f1db39
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-04-20 20:44:37 +0900

    Study: Intent

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    int REQUEST_CODE = 1000;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("lifeCycle", "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         *  명시적 Intent
+         Intent intent = new Intent(this, secondActivity.class);
+         intent.putExtra("intent_key", 5);
+         intent.putExtra("intent_key_string", "string");
+         startActivity(intent);
+         */
+
+        /**
+         * 암시적 Intent
+         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https:google.com"));
+         startActivity(intent);
+         */
+
+        Intent intent = new Intent(this, secondActivity.class);
+        intent.putExtra("intent_key", 5);
+        intent.putExtra("intent_key_string", "string");
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == 200) {
+                String result = data.getStringExtra("result");
+                Log.d("onActivityResult", "result : " + result);
+            } else if (resultCode == 300) {
+                Log.d("onActivityResult", "실패");
+            }
+        }
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("lifeCycle", "1: onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("lifeCycle", "1: onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("lifeCycle", "1: onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("lifeCycle", "1: onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("lifeCycle", "1: onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("lifeCycle", "1: onRestart");
+        super.onRestart();
+    }
+}
---
+++ MyApplication2/app/src/main/java/com/example/myapplication/secondActivity.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class secondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("lifeCycle", "2: onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        /**
+         Intent intent = getIntent();
+         int data = intent.getIntExtra("intent_key", 0);
+         String stringData = intent.getStringExtra("intent_key_string");
+         Log.d("intent_key", "" + data);
+         Log.d("intent_key", "" + stringData);
+         */
+
+        /**
+         * 작업을 마친 후
+         */
+        Intent intent = new Intent();
+        intent.putExtra("result", "성공");
+        setResult(300, intent);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("lifeCycle", "2: onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("lifeCycle", "2: onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("lifeCycle", "2: onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("lifeCycle", "2: onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("lifeCycle", "2: onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("lifeCycle", "2: onRestart");
+        super.onRestart();
+    }
+}
