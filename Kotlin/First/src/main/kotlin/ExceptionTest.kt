51f14a5b56dfe68738d1c49b8fa47ac3a0e732ee
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-04 19:22:27 +0900

    Study: Kotlin - Exception

---
+++ Kotlin/First/src/main/kotlin/ExceptionTest.kt
@@ -0,0 +1,35 @@
+fun main(args: Array<String>) {
+
+    // java와 달리 try catch가 필수가 아니다.
+    var divNumber = 0
+    try {
+        707 / divNumber
+    } catch (e: Exception) {
+        println(e)
+    } finally {
+        println("Last")
+    }
+
+    // 1. Kotlin Style (예외처리는 소스코드에서 로직으로! 아니면 종료)
+    var zeroDivided = 707
+    println(zeroDivided)
+
+    // null checking은 까다로움
+    var addNumber : Int? = null
+
+    // 에러
+//    var sum = addNumber + 100
+
+    // 처리방법 1
+//    if (addNumber != null) {
+//        var sum = addNumber +100
+//    }
+
+    // 처리방법 2 (종료)
+//    var sum = addNumber!! + 100
+
+    // 처리방법 3(실행안됨)
+    var sum = addNumber?.let {
+        it + 100
+    }
+}
