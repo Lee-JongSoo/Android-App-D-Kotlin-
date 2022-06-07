0601dd985395c46a6dc5718a9c78e502e7a5ad84
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-07 16:03:05 +0900

    Feat: 권한 얻기

--- Kotlin-projects/project1/app/src/main/java/com/example/project1/MainActivity.kt
+++ Kotlin-projects/project1/app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.project1
 
+import android.Manifest.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
+    private val CAMERA_PERMISSION_REQUEST = 1000
+    private val GALLERY_PERMISSION_REQUEST = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +26,33 @@ class MainActivity : AppCompatActivity() {
                     override fun cameraOnclick() {
                         Log.d("upload", "cameraOnClick")
 
-                        // 권한 작업을 진행
+                        // 권한 작업을 진행 - camera
+                        checkCameraPermission()
                     }
 
                     override fun galleryOnclick() {
                         Log.d("upload", "galleryOnClick")
 
-                        // 권한 작업을 진행
+                        // 권한 작업을 진행 - gallery
+                        checkGalleryPermission()
                     }
                 })
             }.show(supportFragmentManager, "")
         }
+    }
+
+    private fun checkCameraPermission(){
+        PermissionUtil().requestPermission(
+            this, CAMERA_PERMISSION_REQUEST, permission.CAMERA
+        )
+    }
+
+    private fun checkGalleryPermission(){
+        PermissionUtil().requestPermission(
+            this,
+            GALLERY_PERMISSION_REQUEST,
+            permission.READ_EXTERNAL_STORAGE,
+            permission.CAMERA
+        )
     }
 }
