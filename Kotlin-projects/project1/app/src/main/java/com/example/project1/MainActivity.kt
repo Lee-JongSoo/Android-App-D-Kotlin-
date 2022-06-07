871ead23674e4dc131bf3b32841c122ee6532228
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-07 13:02:53 +0900

    Feat: onClickListener를 interface를 활용하여 코딩

--- Kotlin-projects/project1/app/src/main/java/com/example/project1/MainActivity.kt
+++ Kotlin-projects/project1/app/src/main/java/com/example/project1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +16,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupListener() {
         upload_image.setOnClickListener {
-            UploadChooser().show(supportFragmentManager, "")
+            UploadChooser().apply {
+                addNotifier(object : UploadChooser.UploadChooserNotifierInterface{
+                    override fun cameraOnclick() {
+                        Log.d("upload", "cameraOnClick")
+
+                        // 권한 작업을 진행
+                    }
+
+                    override fun galleryOnclick() {
+                        Log.d("upload", "galleryOnClick")
+
+                        // 권한 작업을 진행
+                    }
+                })
+            }.show(supportFragmentManager, "")
         }
     }
 }
--- Kotlin-projects/project1/app/src/main/java/com/example/project1/UploadChooser.kt
+++ Kotlin-projects/project1/app/src/main/java/com/example/project1/UploadChooser.kt
@@ -5,8 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.upload_chooser.*
 
 class UploadChooser : BottomSheetDialogFragment() {
+
+    interface UploadChooserNotifierInterface {
+        fun cameraOnclick()
+        fun galleryOnclick()
+    }
+
+    var uploadChooserNotifierInterface : UploadChooserNotifierInterface? = null
+
+    fun addNotifier(listener: UploadChooserNotifierInterface) {
+        uploadChooserNotifierInterface  = listener
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +30,16 @@ class UploadChooser : BottomSheetDialogFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        upload_camera.setOnClickListener{
+            uploadChooserNotifierInterface?.cameraOnclick()
+        }
+
+        upload_gallery.setOnClickListener {
+            uploadChooserNotifierInterface?.galleryOnclick()
+        }
     }
 }
