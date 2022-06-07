2b30e99b5e35938b1990f189ba7e93f2a757ef18
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-08 07:11:36 +0900

    Feat: create Asynctask

--- Kotlin-projects/project1/app/src/main/java/com/example/project1/MainActivity.kt
+++ Kotlin-projects/project1/app/src/main/java/com/example/project1/MainActivity.kt
@@ -5,15 +5,23 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import androidx.core.content.FileProvider
+import com.google.api.services.vision.v1.Vision
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.main_analyze_view.*
 import java.io.File
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.lang.ref.WeakReference
+import java.net.CacheResponse
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -117,6 +125,53 @@ class MainActivity : AppCompatActivity() {
         val bitmap: Bitmap = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
         uploaded_image.setImageBitmap(bitmap)
         uploadChooser?.dismiss()
+        requestCloudVisionApi(bitmap)
+    }
+
+    private fun requestCloudVisionApi(bitmap: Bitmap) {
+
+    }
+
+    inner class ImageRequestTask constructor(
+        activity: MainActivity,
+        val request: Vision.Images.Annotate
+    ) : AsyncTask<Any, Void, String>() {
+
+        private val weakReference: WeakReference<MainActivity>
+
+        init {
+            weakReference =  WeakReference(activity)
+        }
+
+
+        override fun doInBackground(vararg p0: Any?): String {
+            try {
+                val response = request.execute()
+                return convertResponseToString(response)
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return "분석 실패"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+    }
+
+    private fun convertResponseToString(response: BatchAnnotateImagesResponse): String {
+        val message = StringBuilder("분설 결과\n")
+        val labels = response.responses[0].labelAnnotations
+
+        labels?.let {
+            it.forEach {
+                message.append(String.format(Locale.US, "%.3f: %s", it.score, it.description))
+                message.append("\n")
+            }
+            return message.toString()
+        }
+        return "분석 실패"
     }
 
     private fun createCameraFile(): File {
