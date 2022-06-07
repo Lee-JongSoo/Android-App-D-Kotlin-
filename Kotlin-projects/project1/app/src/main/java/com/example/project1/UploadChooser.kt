f3d077170237afdcd28610fd06e480feec0758ef
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-07 12:42:26 +0900

    Feat: 다이얼로그 만들기

---
+++ Kotlin-projects/project1/app/src/main/java/com/example/project1/UploadChooser.kt
@@ -0,0 +1,22 @@
+package com.example.project1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class UploadChooser : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.upload_chooser, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
