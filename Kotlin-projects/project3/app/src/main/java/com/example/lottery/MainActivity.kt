3c182ef49e1d9e7751349a31e31d8e047d72696d
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-23 22:35:24 +0900

    Feat: shape drawable를 이용하여 디자인

--- Kotlin-projects/project3/app/src/main/java/com/example/lottery/MainActivity.kt
+++ Kotlin-projects/project3/app/src/main/java/com/example/lottery/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -88,6 +89,8 @@ class MainActivity : AppCompatActivity() {
             textView.isVisible = true
             textView.text = numberPicker.value.toString()
 
+            setNumberBackground(numberPicker.value, textView)
+
             pickNumberSet.add(numberPicker.value)
         }
 
@@ -104,9 +107,19 @@ class MainActivity : AppCompatActivity() {
 
                 textView.text = number.toString()
                 textView.isVisible = true
-            }
 
-            Log.d("MainActivity", list.toString())
+                setNumberBackground(number, textView)
+            }
+        }
+    }
+
+    private fun setNumberBackground(number: Int, textView: TextView) {
+        when (number) {
+            in 1..10 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_yellow)
+            in 11..20 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_blue)
+            in 21..30 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_red)
+            in 31..40 -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_gray)
+            else -> textView.background = ContextCompat.getDrawable(this, R.drawable.circle_green)
         }
     }
 
